f1772cbfe80bf20b73cbe1467fc193ce20d1f064
Ruslan <115139612+Ruslan2806@users.noreply.github.com>
2024-07-13 23:00:15 +0300

    Add files via upload
    implemented tile construction and moving it along the screen

---
+++ TETRIS/desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setWindowedMode(476, 900); // установка фиксированного размера окна
+		//config.setResizable(false); // запрет на изменение размеров окна
+		config.setTitle("TETRIS");
+		new Lwjgl3Application(new Tetris(), config);
+	}
+}
